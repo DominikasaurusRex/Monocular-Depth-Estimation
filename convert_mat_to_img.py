819616cc8c0aef7d86db772e0fad4830bb06870b
KomodoFactory <github@komodofactory.com>
2017-06-19 15:28:50 +0200

    refactored convert_mat_to_img

--- convert_mat_to_img.py
+++ convert_mat_to_img.py
@@ -1,4 +1,3 @@
-#encoding: utf-8
 import os
 import numpy as np
 import h5py 
@@ -6,33 +5,40 @@ from PIL import Image
 import random
 
 
-def convert_nyu(path):
-    print("load dataset: %s" % (path))
-    f = h5py.File(path)
+def convert_nyu_dataset_into_images_and_csv(path):
+    print("loading dataset: %s" % (path))
+    nyu_dataset_file  = h5py.File(path)
 
 
-    trains = []
-    for i, (image, depth) in enumerate(zip(f['images'], f['depths'])):
-        ra_image = image.transpose(2, 1, 0)
-        ra_depth = depth.transpose(1, 0)
-        re_depth = (ra_depth/np.max(ra_depth))*255.0
-        image_pil = Image.fromarray(np.uint8(ra_image))
-        depth_pil = Image.fromarray(np.uint8(re_depth))
-        image_name = os.path.join("data", "nyu_datasets", "%05d.jpg" % (i))
-        image_pil.save(image_name)
-        depth_name = os.path.join("data", "nyu_datasets", "%05d.png" % (i))
-        depth_pil.save(depth_name)
+    trainingsmaterial = []
+    #create the images and depth maps
+    #also add all the paths to trainingsmaterial
+    for i, (image_original, image_depth) in enumerate(zip(nyu_dataset_file['images'], nyu_dataset_file['depths'])):
+        image_original_transpose = image_original.transpose(2, 1, 0)
+        image_depth_transpose = image_depth.transpose(1, 0)
+        image_depth_transpose = (image_depth_transpose/np.max(image_depth_transpose))*255.0
+        image_original_pil = Image.fromarray(np.uint8(image_original_transpose))
+        image_depth_pil = Image.fromarray(np.uint8(image_depth_transpose))
+        image_original_name = os.path.join("data", "nyu_datasets", "%05d.jpg" % (i))
+        image_original_pil.save(image_original_name)
+        image_depth_name = os.path.join("data", "nyu_datasets", "%05d.png" % (i))
+        image_depth_pil.save(image_depth_name)
 
-        trains.append((image_name, depth_name))
+        trainingsmaterial.append((image_original_name, image_depth_name))
 
-    random.shuffle(trains)
+    random.shuffle(trainingsmaterial)
+    
+    write_csv_file(trainingsmaterial)
 
+def write_csv_file(trainingsmaterial):
     with open('train.csv', 'w') as output:
-        for (image_name, depth_name) in trains:
-            output.write("%s,%s" % (image_name, depth_name))
+        for (image_original_path, image_depth_path) in trainingsmaterial:
+            output.write("%s,%s" % (image_original_path, image_depth_path))
             output.write("\n")
+
+
 
 if __name__ == '__main__':
     current_directory = os.getcwd()
-    nyu_path = 'data/nyu_depth_v2_labeled.mat'
-    convert_nyu(nyu_path)
+    nyu_dataset_path = 'data/nyu_depth_v2_labeled.mat'
+    convert_nyu_dataset_into_images_and_csv(nyu_dataset_path)
